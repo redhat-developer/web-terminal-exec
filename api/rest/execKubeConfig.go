b7eca629d4e1d95a7d0b517be99e032d0ee5bdaf
Sergii Leshchenko <sleshche@redhat.com>
2020-06-30 18:28:30 +0300

    Sync with upstream https://github.com/eclipse/che-machine-exec @ f95b892 (7.15.0)
    Ignore:
    - .github
    - README.md

---
+++ api/rest/execKubeConfig.go
@@ -0,0 +1,57 @@
+//
+// Copyright (c) 2012-2019 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
+package rest
+
+import (
+	"github.com/eclipse/che-machine-exec/auth"
+	"github.com/eclipse/che-machine-exec/common/rest"
+	"net/http"
+
+	"github.com/eclipse/che-machine-exec/api/model"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func HandleKubeConfig(c *gin.Context) {
+	var token string
+	if auth.IsEnabled() {
+		var err error
+		token, err = auth.Authenticate(c)
+		if err != nil {
+			rest.WriteErrorResponse(c, err)
+			return
+		}
+	}
+
+	var initConfigParams model.InitConfigParams
+	if c.BindJSON(&initConfigParams) != nil {
+		rest.WriteResponse(c, http.StatusInternalServerError, "Failed to convert body args into internal structure")
+		return
+	}
+
+	if initConfigParams.ContainerName == "" {
+		c.Writer.WriteHeader(http.StatusBadRequest)
+		_, err := c.Writer.Write([]byte("Container name is required"))
+		if err != nil {
+			logrus.Error("Failed to write error response", err)
+		}
+		return
+	}
+
+	err := HandleKubeConfigCreation(c, &initConfigParams, token)
+
+	if err != nil {
+		logrus.Errorf("Unable to create kubeconfig. Cause: %s", err.Error())
+		rest.WriteResponse(c, http.StatusInternalServerError, err.Error())
+	}
+}
