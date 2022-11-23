6735ccf922faa34034f29459e1fea3f756201980
Angel Misevski <amisevsk@redhat.com>
2022-11-23 13:34:05 -0500

    Adapt auth package to verify user access
    When receiving a request, check the X-Access-Token and
    X-Forwarded-Access-Token headers to verify that user making the request
    matches the configured authorized user ID
    
    Adapted from a6a14c0
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

---
+++ pkg/auth/token.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2019-2022 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const (
+	accessTokenHeader          = "X-Access-Token"
+	forwardedAccessTokenHeader = "X-Forwarded-Access-Token"
+)
+
+func ExtractToken(r *http.Request) (string, error) {
+	token := r.Header.Get(accessTokenHeader)
+	if token != "" {
+		token = strings.TrimPrefix(token, "Bearer ")
+		return token, nil
+	}
+
+	token = r.Header.Get(forwardedAccessTokenHeader)
+	if token != "" {
+		return token, nil
+	}
+
+	return "", fmt.Errorf("authorization header is missing")
+}
---
+++ pkg/auth/user.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2019-2022 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+
+package auth
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/redhat-developer/web-terminal-exec/pkg/config"
+	"github.com/redhat-developer/web-terminal-exec/pkg/operations"
+	"github.com/sirupsen/logrus"
+)
+
+func Authenticate(r *http.Request) error {
+	token, err := ExtractToken(r)
+	if err != nil {
+		return err
+	}
+	uid, err := operations.GetCurrentUserUID(token)
+	if err != nil {
+		return fmt.Errorf("unable to verify user: %s", err)
+	}
+	if uid != config.AuthenticatedUserID {
+		logrus.Debugf("User failed to authenticate: authorized user = '%s', requested user = '%s'", config.AuthenticatedUserID, uid)
+		return fmt.Errorf("the current user is not authorized to access this web terminal")
+	}
+	logrus.Debugf("User '%s' authenticated", uid)
+	return nil
+}
